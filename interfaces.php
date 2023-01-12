ca13904acc20cfd892a1fc5bf036850e8bc640c6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-12 09:25:20 +0600

    interfaces example

--- interfaces.php
+++ interfaces.php
@@ -2,8 +2,21 @@
 
 // interfaces allow you to specify what methods a class should implement. interfaces are declared with the interface keyword.
 
-interface InterfaceName {
-  public function someMethod1();
-  public function someMethod2($name, $color);
-  public function someMethod3() : string;
+// interface InterfaceName {
+//   public function someMethod1();
+//   public function someMethod2($name, $color);
+//   public function someMethod3() : string;
+// }
+
+interface Animal {
+  public function makeSound();
 }
+
+class Cat implements Animal {
+  public function makeSound() {
+    echo "Meow";
+  }
+}
+
+$animal = new Cat();
+$animal->makeSound();
