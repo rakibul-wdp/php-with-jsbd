515c157595c333e5f59945c74fa8b5bfec158ef0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-12 09:55:20 +0600

    interface with more example

--- interfaces.php
+++ interfaces.php
@@ -8,15 +8,39 @@
 //   public function someMethod3() : string;
 // }
 
+// Interface definition
 interface Animal {
   public function makeSound();
 }
 
+// class definitions
 class Cat implements Animal {
   public function makeSound() {
     echo "Meow";
   }
 }
 
-$animal = new Cat();
-$animal->makeSound();
+class Dog implements Animal {
+  public function makeSound() {
+    echo " Bark ";
+  }
+}
+
+class Mouse implements Animal {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+// create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach($animals as $animal) {
+  $animal->makeSound();
+}
+
+// cat, dog and mouse are all classes that implement the animal interface, which means that all of them are able to make a sound using the makeSound() method.
