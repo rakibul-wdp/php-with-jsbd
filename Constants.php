dd64d913c308bef3fc35ed8b1a2af7d4e83405b9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-20 16:07:56 +0600

    learned constant in php

---
+++ Constants.php
@@ -0,0 +1,23 @@
+<?php
+
+// define()
+// const keyword
+
+// define('NUMBER', 50);
+// define('NUMBER', 50);
+
+$number = 50;
+$number = 70;
+
+// const NUMBER = 50;
+// const NUMBER = 70;
+
+// echo NUMBER;
+
+// echo $number;
+
+// Constants don't follow any variable scoping rules.
+// They can be defined and accessed fro anywhere.
+
+// const -> global
+// variable -> global, local, static
