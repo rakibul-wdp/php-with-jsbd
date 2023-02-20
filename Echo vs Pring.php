d2ecfed3f63ecdd8d673ac4beec3ad1570306bec
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-20 15:14:17 +0600

    comment in php

--- Echo vs Pring.php
+++ Echo vs Pring.php
@@ -1,5 +1,19 @@
 <?php
 
-echo ("Hello");
-print(" World");
-print ("hello". "this" . "is");
+// echo ("Hello");
+// print(" World");
+// print ("hello". "this" . "is");
+
+// $var = echo("hello");
+
+// single line comment
+# this is also single line comment
+
+/**
+ * this 
+ * is
+ * multiline
+ * comment
+ */
+
+echo "hello";
