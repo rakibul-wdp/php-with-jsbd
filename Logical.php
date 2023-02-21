1f4a221d15493c522c91062d154dfb651dc018fb
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-21 12:18:10 +0600

    logical operator

---
+++ Logical.php
@@ -0,0 +1,14 @@
+<?php
+
+$var1 = 10;
+$var2 = 15;
+
+// var_dump($var1 and $var2); // And Operator
+// var_dump($var1 && $var2); // And Operator
+// var_dump($var1 or $var2); // Or Operator
+// var_dump($var1 || $var2); // Or Operator
+// var_dump($var1 xor $var2); // True if either one of operand is true
+
+// var_dump(!$var1); // Not True change
+
+var_dump($var1 == 10 || $var2 < 5); // And Operator
