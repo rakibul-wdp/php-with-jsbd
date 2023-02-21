ff6025abd334a96189d08b14c6ccbb0fd328822c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-21 11:48:51 +0600

    arithmetic and assignment operator

---
+++ Arithmetic.php
@@ -0,0 +1,25 @@
+<?php
+
+// Arithmetic Operators
+$num1 = 10;
+$num2 = 2;
+
+// $num1 += $num2; // $num1 = $num1 + $num2
+// $num1 -= $num2; // $num1 = $num1 - $num2
+// $num1 *= $num2; // $num1 = $num1 * $num2
+$num1 %= $num2; // $num1 = $num1 % $num2
+
+echo $num1;
+
+// $result = $num1 + $num2; // Addition
+// $result = $num1 - $num2; // Subtraction
+// $result = $num1 * $num2; // Multiplication
+// $result = $num1 / $num2; // Division
+// $result = $num1 % $num2; // Modulus
+// $result = $num1 ** $num2; // Exponentiation
+
+// echo $result;
+
+// Assignment Operators
+
+// The left operand gets set to the value of the expression on the right
