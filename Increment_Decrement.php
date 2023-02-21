d5a131dc6f9dd8dab2fccf9090ec20b358cf6b9d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-21 16:33:23 +0600

    increment decrement

---
+++ Increment_Decrement.php
@@ -0,0 +1,19 @@
+<?php
+
+// Increment
+// $var1 = 10;
+
+// echo $var1++;
+// print $var1++;
+// $result = $var1 + $var1++;
+// $result = ++$var1;
+
+// echo $result;
+
+// Decrement
+
+$var1 = 10;
+
+$result = --$var1; // $var1 - 1
+
+echo $result;
