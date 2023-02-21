4130ca6dc01e338669982cac2fabd5561be3c740
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-21 12:33:11 +0600

    if else in php

---
+++ Condition.php
@@ -0,0 +1,18 @@
+<?php
+
+// $t = date("H");
+// echo $t;
+
+// if ($t < "20") {
+//   echo "Have a good day";
+// }
+
+$t = 25;
+
+if ($t < "20") {
+  echo "have a good day!";
+} elseif ($t > 20) {
+  echo "print elseif condition";
+}else {
+  echo "Have a good night";
+}
