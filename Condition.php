1a1190cc216cb033867fad2b3e87d09e55dc510b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-21 12:36:49 +0600

    switch condition

--- Condition.php
+++ Condition.php
@@ -7,12 +7,28 @@
 //   echo "Have a good day";
 // }
 
-$t = 25;
+// $t = 25;
 
-if ($t < "20") {
-  echo "have a good day!";
-} elseif ($t > 20) {
-  echo "print elseif condition";
-}else {
-  echo "Have a good night";
+// if ($t < "20") {
+//   echo "have a good day!";
+// } elseif ($t > 20) {
+//   echo "print elseif condition";
+// }else {
+//   echo "Have a good night";
+// }
+
+$fav_color = "green";
+
+switch ($fav_color) {
+  case "red":
+    echo "your fav color is red";
+    break;
+  case "blue":
+    echo "your fav color is blue";
+    break;
+  case "green":
+    echo "your fav color is green";
+    break;
+  default:
+    echo "your don't have fav color";
 }
