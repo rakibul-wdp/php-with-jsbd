b503a3e8fda4d3bff0881cae49a013fbcda8d388
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-22 16:27:16 +0600

    array

---
+++ arrays.php
@@ -0,0 +1,31 @@
+<?php
+
+$cars = array("volvo", "BMW", "Toyota");
+echo rsort($cars);
+// echo "I like $cars[0], $cars[1], $cars[2]";
+// $arr_length = count($cars);
+
+// for($x = 0; $x < count($cars); $x++) {
+//   echo $cars[$x];
+//   echo PHP_EOL;
+// }
+
+// $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+// echo $age["Peter"];
+
+// foreach($age as $x => $x_value) {
+//   echo $x, $x_value;
+//   echo PHP_EOL;
+// }
+
+// $cars = array(
+//   array("Volvo", 22, 18),
+//   array("BMW", 15, 13),
+//   array("Saab", 5, 2),
+//   array("Land Rover", 17, 15)
+// );
+
+// echo $cars[0][0] . $cars[0][1] . $cars[0][2];
+
+// $numbers = array(4, 6, 2, 22, 11);
+// echo sort($numbers);
