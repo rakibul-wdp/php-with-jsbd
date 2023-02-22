88c3a03a40255f7946627c740fccac97d5510fcd
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-22 17:21:26 +0600

    switch in php

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+
+$language = "PHP";
+
+switch ($language) {
+  case "PHP":
+    echo "my fav language is PHP";
+    break;
+  case "JS":
+    echo "My fav language is JS";
+    break;
+  case "TJ":
+    echo "my fav lan is TJ";
+  default:
+    echo "I don't like ";
+}
