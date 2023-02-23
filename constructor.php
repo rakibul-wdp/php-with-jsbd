8a6cca7dc41791e82a769f499022e8bf6532d71d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-23 18:07:47 +0600

    access modifiers

--- constructor.php
+++ constructor.php
@@ -23,19 +23,53 @@
 // echo $apple->get_color();
 // echo PHP_EOL;
 
+// class Fruit {
+//   public $name;
+//   public $color;
+
+//   function __construct($name, $color)
+//   {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+//   function __destruct()
+//   {
+//     echo "The fruit is {$this->name} and the color is {$this->color}";
+//   }
+// }
+
+// $apple = new Fruit("Apple", "red");
+
+// PHP OOP - Access Modifiers
+
+// class Fruit{
+//   public $name;
+//   protected $color;
+//   private $weight;
+// }
+
+// $mango = new Fruit();
+// $mango->name = "Mango"; // OK
+// $mango->color = "Yellow"; // ERROR
+// $mango->weight = "300"; // ERROR
+
 class Fruit {
   public $name;
   public $color;
+  public $weight;
 
-  function __construct($name, $color)
-  {
-    $this->name = $name;
-    $this->color = $color;
+  function set_name($n) { // a public function (default)
+    $this->name = $n;
   }
-  function __destruct()
-  {
-    echo "The fruit is {$this->name} and the color is {$this->color}";
+  protected function set_color($n) { // a protected function
+    $this->color = $n;
+  }
+  private function set_weight($n) { // a private function
+    $this->weight = $n;
   }
 }
 
-$apple = new Fruit("Apple", "red");
+$mango = new Fruit();
+$mango->set_name("Mango");
+$mango->set_color("Yellow");
+$mango->set_wight("300");
