a69ef18ff56ee90724724b2357dff7b8bbeaa4be
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-23 17:58:29 +0600

    destruct in php

--- constructor.php
+++ constructor.php
@@ -1,5 +1,28 @@
 <?php
 
+// class Fruit {
+//   public $name;
+//   public $color;
+
+//   function __construct($name, $color)
+//   {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+//   function get_name() {
+//     return $this->name;
+//   }
+//   function get_color() {
+//     return $this->color;
+//   }
+// }
+
+// $apple = new Fruit("Apple", "red");
+// echo $apple->get_name();
+// echo PHP_EOL;
+// echo $apple->get_color();
+// echo PHP_EOL;
+
 class Fruit {
   public $name;
   public $color;
@@ -9,16 +32,10 @@ class Fruit {
     $this->name = $name;
     $this->color = $color;
   }
-  function get_name() {
-    return $this->name;
-  }
-  function get_color() {
-    return $this->color;
+  function __destruct()
+  {
+    echo "The fruit is {$this->name} and the color is {$this->color}";
   }
 }
 
 $apple = new Fruit("Apple", "red");
-echo $apple->get_name();
-echo PHP_EOL;
-echo $apple->get_color();
-echo PHP_EOL;
