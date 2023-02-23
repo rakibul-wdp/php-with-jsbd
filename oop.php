38ed3e1effdecd2947850ee519e4cbcbb3d1688a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-23 16:36:28 +0600

    define objects

--- oop.php
+++ oop.php
@@ -14,4 +14,22 @@ class Fruit {
   function get_name() {
     return $this -> name;
   }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
 }
+
+$apple = new Fruit();
+// $banana = new Fruit();
+$apple->set_name("Apple");
+$apple->set_color("Red");
+// $banana->set_name("Banana");
+
+echo "Name: " . $apple->get_name();
+echo PHP_EOL;
+// echo $banana->get_name();
+echo "Color: " . $apple->get_color();
+echo PHP_EOL;
