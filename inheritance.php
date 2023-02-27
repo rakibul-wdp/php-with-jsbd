cf78bdc1bbce4a438fa197424f4e084791308a53
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-27 14:46:35 +0600

    protedted access modifier

--- inheritance.php
+++ inheritance.php
@@ -2,24 +2,50 @@
 
 // Inheritance in OOP = When a class derives from another class.
 
+// class Fruit {
+//   public $name;
+//   public $color;
+//   public function __construct($name, $color) {
+//     $this->name = $name;
+//     $this->color = $color;
+//   }
+//   public function intro() {
+//     echo "The fruit is {$this->name} and the color is {$this->color}";
+//   }
+// }
+
+// // Strawberry is inherited from Fruit
+// class Strawberry extends Fruit {
+//   public function message() {
+//     echo "Am I a fruit or berry?";
+//   }
+// }
+// $strawberry = new Strawberry("Strawberry", "red");
+// $strawberry->message();
+// $strawberry->intro();
+
+// PHP - Inheritance and the Protected Access Modifier
+
 class Fruit {
   public $name;
   public $color;
-  public function __construct($name, $color) {
+  public function __construct($name, $color)
+  {
     $this->name = $name;
     $this->color = $color;
   }
-  public function intro() {
+  protected function intro() {
     echo "The fruit is {$this->name} and the color is {$this->color}";
   }
 }
 
-// Strawberry is inherited from Fruit
 class Strawberry extends Fruit {
   public function message() {
-    echo "Am I a fruit or berry?";
+    echo "am i a fruit or a berry";
   }
 }
-$strawberry = new Strawberry("Strawberry", "red");
-$strawberry->message();
-$strawberry->intro();
+
+// Try to call all three methods from outside class
+$strawberry = new Strawberry("Strawberry", "red"); // OK. __construct() is public
+$strawberry->message(); // OK. is public
+// $strawberry->intro(); // ERROR. intro() is protedted
