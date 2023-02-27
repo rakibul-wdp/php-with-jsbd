4ff1dca9dd4b404fa7200442781e2d1dfd13a382
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-27 16:18:24 +0600

    abstract class with example

--- abstract_classes.php
+++ abstract_classes.php
@@ -8,8 +8,49 @@ An abstract class or method is defined with the abstract keyword:
 
 */
 
-abstract class ParentClass {
-  abstract public function someMethod1();
-  abstract public function someMethod2($name, $color);
-  abstract public function someMethod3() : string;
+// abstract class ParentClass {
+//   abstract public function someMethod1();
+//   abstract public function someMethod2($name, $color);
+//   abstract public function someMethod3() : string;
+// }
+
+// Parent class
+abstract class Car {
+  public $name;
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
+  abstract public function intro() : string;
 }
+
+// child classes
+class Audi extends Car {
+  public function intro() : string {
+    return "Choose German Quality ? I'm an $this->name";
+  }
+}
+
+class Volvo extends Car {
+  public function intro() : string {
+    return "proud to be swedish I'm a $this->name";
+  }
+}
+
+class Citroen extends Car {
+  public function intro() : string {
+    return "french extravagance I'm a $this->name";
+  }
+}
+
+// create objects form the child classes
+$audi = new audi("Audi");
+echo $audi->intro();
+echo PHP_EOL;
+
+$volvo = new volvo("Volvo");
+echo $volvo->intro();
+echo PHP_EOL;
+
+$citroen = new citroen("citroen");
+echo $citroen->intro();
